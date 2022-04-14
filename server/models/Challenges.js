61370acd4d453e63a7d8878d68005cf91487495f
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-04-13 20:07:03 -0700

    updated hunt/challenge models

--- server/models/Challenges.js
+++ server/models/Challenges.js
@@ -1,9 +1,28 @@
 const { Schema, model } = require('mongoose');
 
-const huntsSchema = new Schema ({
+const challengeSchema = new Schema ({
+    challengename: {
+    type: String,
+    required: true,
+    trim: true,
+    },
+
+    location: {
+        type: [String],
+        required: false,
+    },
+
+    todo: {
+        type: String,
+        required: false,
+    },
+
+    check: {
+        type: Boolean,
+    }
 
 });
 
-const Hunts = model('Hunts', huntsSchema);
+const Challenge = model('Challenge', ChallengeSchema);
 
-module.exports = Hunts;
+module.exports = Challenge;
--- server/models/Hunts.js
+++ server/models/Hunts.js
@@ -1,16 +1,17 @@
 const { Schema, model } = require('mongoose');
 
-const huntsSchema = new Schema ({
+const huntSchema = new Schema ({
     huntname: {
         type: String,
         required: true,
         trim: true,
     },
-    challenges: {
-        Schemas: 
-    },
+    challenges: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Challenge',
+    }],
 });
 
-const Hunts = model('Hunts', huntsSchema);
+const Hunt = model('Hunts', huntSchema);
 
-module.exports = Hunts;
+module.exports = Hunt;
