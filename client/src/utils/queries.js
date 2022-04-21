274045742dd4c718719d1e6965cad88cda99b590
Shane Wiens <Shanewiens@gmail.com>
2022-04-20 18:34:37 -0700

    remote merge

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -44,3 +44,46 @@ export const QUERY_SINGLE_HUNTS = gql`
     }
   }
 `;
+export const QUERY_CHALLENGES = gql`
+  query getChallenges {
+    hunts {
+      _id
+      huntName
+      challenges {
+        challengeId
+        challengeName
+        location {
+          address1
+          address2
+          city
+          state
+          zipCode
+        }
+        todo
+        check
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_CHALLENGE = gql`
+  query getSingleChallenge($huntId: ID!) {
+    hunt(huntId: $huntId) {
+      _id
+      huntName
+      challenges(challengeId: $challengeId) {
+        challengeId
+        challengeName
+        location {
+          address1
+          address2
+          city
+          state
+          zipCode
+        }
+        todo
+        check
+      }
+    }
+  }
+`;
